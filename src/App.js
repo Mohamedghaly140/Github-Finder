8e92757df6f8f5c3f40bf24a114c67cd83fb8ce2
mohamedghaly140 <mohamedghaly140@gmail.com>
2020-06-28 21:16:04 +0200

    creating user and userItem compentent and fetcging data in the app component and passing it to user component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+import Navbar from './Components/Layout/Navbar';
+import Users from './Components/Users/Users';
 import './App.css';
 
-import Navbar from './Components/Layout/Navbar';
-
 class App extends Component {
-  render () {
+  state = {
+    users: [],
+    loading: false
+  }
+
+  componentDidMount () {
+    this.setState({loading: true});
+    axios.get('https://api.github.com/users').then(response => {
+      this.setState({users: response.data, loading: false});
+    }).catch(err => console.log(err));
+  }
+
+  render() {
     return (
-      <div className="App">
+      <div className='App'>
         <Navbar />
+        <div className='container'>
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
       </div>
     );
   }
